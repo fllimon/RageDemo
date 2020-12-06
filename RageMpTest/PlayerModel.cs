85bdc1195747b5fd95b803b87457ff61c6277205
Artur Sharaenko <49069796+fllimon@users.noreply.github.com>
2020-12-06 22:59:43 +0200

    Add class PlayerModel

---
+++ RageMpTest/PlayerModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using GTANetworkAPI;
+
+namespace RageMpTest
+{
+    class PlayerModel : Script
+    {
+        public long Id { get; set; }
+
+        public string PlayerLogin { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string SocialName { get; set; }
+
+        public string Password { get; set; }
+
+        public string Email { get; set; }
+
+        public byte PlayerHealth { get; set; }
+
+        public byte PlayerArmor { get; set; }
+
+    }
+}
