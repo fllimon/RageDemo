bdd9245ffd6ac1c067d571ebb1aaf2eddcc77000
Artur Sharaenko <49069796+fllimon@users.noreply.github.com>
2020-12-06 18:00:42 +0200

    Add project files.

---
+++ RageMpTest/Main.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using GTANetworkAPI;
+
+namespace RageMpTest
+{
+    class Main : Script
+    {
+        [ServerEvent(Event.PlayerConnected)]
+        public void GetPlayerConnected(Player somePlayer)
+        {
+            Console.WriteLine($"{somePlayer.SocialClubName} connected the server!");
+        }
+    }
+}
