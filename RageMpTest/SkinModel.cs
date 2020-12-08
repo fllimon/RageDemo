3520db76ddfa7fce78a3db0292d1370c7823fc67
Artur Sharaenko <49069796+fllimon@users.noreply.github.com>
2020-12-08 11:35:42 +0200

    Add class SkinModel

---
+++ RageMpTest/SkinModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RageMpTest
+{
+    class SkinModel
+    {
+        public float NouseWhidth { get; set; }
+        public float NouseHeight { get; set; }
+        public float NouseLenght { get; set; }
+        public float NouseBridge { get; set; }
+        public float NouseTip { get; set; }
+        public float NouseBridgeShift { get; set; }
+        public float BrowWhidth { get; set; }
+        public float BrowHeight { get; set; }
+        public float CheekboneHeight { get; set; }
+        public float CheekboneWhidth { get; set; }
+        public float Cheeks { get; set; }
+        public float Eyes { get; set; }
+        public float Lips { get; set; }
+        public float JawWidth { get; set; }
+        public float JawHeight { get; set; }
+        public float ChinLenght { get; set; }
+        public float ChinPosition { get; set; }
+        public float ChinWhidth { get; set; }
+        public float ChinShape { get; set; }
+        public float NeckWhidth { get; set; }
+    }
+}
