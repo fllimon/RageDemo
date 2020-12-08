7b5674dcd888feb0f1889b1ca40c2f7e8bab699c
Artur Sharaenko <49069796+fllimon@users.noreply.github.com>
2020-12-08 15:25:33 +0200

    Add Registration class

---
+++ RageMpTest/Registration.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GTANetworkAPI;
+
+namespace RageMpTest
+{
+    class Registration : Script
+    {
+        [Command("registration", Alias = "reg", SensitiveInfo = true)]
+        public void GetPlayerRegistration(Player somePlayer, string login, string name, string lastName, string password, string email)
+        {
+            if (!IsEmptyString(login, name, lastName, password, email))
+            {
+                NAPI.Chat.SendChatMessageToPlayer(somePlayer, "~r~ Значение ня могут быть пустыми!");
+            }
+        }
+
+        private bool IsEmptyString(params string[] data)
+        {
+            bool isNotEmpty = false;
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (data[i] == null)
+                {
+                    isNotEmpty = true;
+
+                    break;
+                }
+            }
+
+            return isNotEmpty;
+        } 
+    }
+}
